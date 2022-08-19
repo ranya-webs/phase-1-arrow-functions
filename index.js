cb3e69167e8fe6f83d457891ef7189ebe32cea23
Ranya Al-Jumaili <ranyaosam1@gmail.com>
2022-08-19 19:22:03 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+// This function takes two numbers, a and b, and returns the value of a / b
+const divide = (a, b) => a / b
+
+// This function takes a number, num, and returns its square number (number times itself)
+// {const square = num => Math.pow(num, 2)} gives the same result
+const square = num => num * num
+
+// This function takes two numbers, a and b, and returns the value of a + b
+const add = (a,b) => a + b
